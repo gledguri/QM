99b8cc8d1405ca51e5035462114d9fb18d6bc3f0
Gled <51749738+gledguri@users.noreply.github.com>
2024-08-22 12:06:54 -0700

    Update Package_functions.R

--- R/Package_functions.R
+++ R/Package_functions.R
@@ -20,11 +20,9 @@
 #' # Extract summary statistics for a different parameter, "beta"
 #' param_summary <- extract_param(model = stanMod_1, parmeter = "beta")
 extract_param <- function(model,par){
-	# fit <- summary(model,par)
 	fit <- methods::selectMethod("summary", signature = "stanfit")(object = model, par = par)
-	# fit <- summary(model, par = par)$summary
-	return(fit)
-	# return(fit %>% unlist()%>%as.data.frame%>%round(.,2))
+	fit <- fit$summary
+	return(fit %>% unlist()%>%as.data.frame%>%round(.,3))
 }
 
 #' Extract 2-Dimensional Model parameters as a matrix
