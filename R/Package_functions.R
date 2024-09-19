89ea904524587c79fb4a047e25fdea07c8fe1ce2
Gled <51749738+gledguri@users.noreply.github.com>
2024-09-18 19:43:12 -0700

    Update Package_functions.R

--- R/Package_functions.R
+++ R/Package_functions.R
@@ -1552,6 +1552,47 @@ model {
 }
 "
 
+# M3_2 stan code ------------------------------------------------------------
+M3_2_stan_code <- "
+data {
+	// Intigers
+  int N_sp_i_MC2; // Number of species
+  int N_obs_Y_M5; // Number of samples included in mock community
+  //
+  // Data
+  vector[N_sp_i_MC2] alr_M5; // Additive log(e)-ratio of initial concentration of mock community
+  int Y_M5[N_sp_i_MC2,N_obs_Y_M5]; // Sequence reads of mock community samples
+  int NPCR;  // Number of PCR reactions
+  //
+  // Parameters
+  real tau_eta_mock_sd; // dispersion parameter of eta
+}
+parameters {
+  vector[N_sp_i_MC2-1] alpha_raw;
+}
+transformed parameters{
+  matrix[N_sp_i_MC2,N_obs_Y_M5] gamma_M5;
+  matrix[N_sp_i_MC2,N_obs_Y_M5] psi_M5;
+  vector[N_sp_i_MC2] alpha;
+  alpha[1:(N_sp_i_MC2-1)] = alpha_raw * tau_eta_mock_sd;
+  alpha[N_sp_i_MC2] = 0;
+  for (i in 1:N_obs_Y_M5){
+    for(j in 1:N_sp_i_MC2){
+      gamma_M5[j,i] = alr_M5[j]+(NPCR*(alpha[j]));
+    }
+  }
+  for (i in 1:N_obs_Y_M5){
+    psi_M5[,i] = softmax(gamma_M5[,i]);
+  }
+}
+model {
+  for (i in 1:N_obs_Y_M5){
+    Y_M5[,i] ~ multinomial(psi_M5[,i]);
+  }
+  alpha_raw ~ std_normal();
+}
+"
+
 # M4 stan code ------------------------------------------------------------
 M4_stan_code <- "
 data {
@@ -2010,6 +2051,7 @@ load_model <- function(model = 'M1') {
 		M1 = list(code = M1_stan_code, name = "M1 model"),
 		M2 = list(code = M2_stan_code, name = "M2 model"),
 		M3 = list(code = M3_stan_code, name = "M3 model"),
+		M3_2 = list(code = M3_2_stan_code, name = "M3_2 model"),
 		M4 = list(code = M4_stan_code, name = "M4 model"),
 		M5_1 = list(code = M5_1_stan_code, name = "M5.1 model"),
 		M5_2 = list(code = M5_2_stan_code, name = "M5.2 model")
