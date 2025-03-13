1fb1eee4279222044cb17b9031072468019c5007
Gled <51749738+gledguri@users.noreply.github.com>
2025-03-13 15:01:41 -0700

    update

--- R/Data.R
+++ R/Data.R
@@ -1,4 +1,4 @@
-#' qPCR Dataset Description
+#' Atlantic cod qPCR Dataset Description
 #'
 #' This dataset contains qPCR results, from Guri et al. 2024 (Quantifying the Detection Sensitivity and Precision of qPCR and ddPCR Mechanisms for eDNA Samples).
 #'
@@ -15,10 +15,32 @@
 #' @source qPCR data from Guri et al. 2024 (Quantifying the Detection Sensitivity and Precision of qPCR and ddPCR Mechanisms for eDNA Samples).
 #'
 #' @examples
-#' data(qpcr)
-#' head(qpcr)
-#' summary(qpcr)
-"qpcr"
+#' data(cod_qpcr)
+#' head(cod_qpcr)
+#' summary(cod_qpcr)
+"cod_qpcr"
+
+#' Atlantic herring qPCR Dataset Description
+#'
+#' This dataset contains qPCR results, from Guri et al. 2024 (Quantifying the Detection Sensitivity and Precision of qPCR and ddPCR Mechanisms for eDNA Samples).
+#'
+#' @format A tibble (data frame):
+#' \describe{
+#'   \item{Well}{A character vector indicating the well identifier (e.g., "A1", "B2").}
+#'   \item{Sample_name}{A character vector indicating the name of the sample. Samples with the same names are considered technical replicates.}
+#'   \item{Species}{A character vector indicating the targeted species.}
+#'   \item{Sample_type}{A character vector indicating the task associated with each well (e.g., "STANDARD", "UNKNOWN").}
+#'   \item{Ct}{A character vector with the quantification cycle (Cq) value for each well. "Undetermined" indicates no amplification.}
+#'   \item{Plate}{A character vector indicating the plate in which the samples are run.}
+#'   \item{Std_concentration}{A numeric vector with the DNA concentration of standards. NA for Sample_type == UNKNOWN}
+#' }
+#' @source qPCR data from Guri et al. 2024 (Quantifying the Detection Sensitivity and Precision of qPCR and ddPCR Mechanisms for eDNA Samples).
+#'
+#' @examples
+#' data(herring_qpcr)
+#' head(herring_qpcr)
+#' summary(herring_qpcr)
+"herring_qpcr"
 
 #' metabarcoding Dataset Description
 #'
